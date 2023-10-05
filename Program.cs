f808a472c7e74fda48a9ae98fae15a2e7654813f
albin29 <146028773+albin29@users.noreply.github.com>
2023-10-05 18:13:52 +0200

    added version 1 of basic login with username and passwords stored in .csv file

--- Program.cs
+++
@@ -1,6 +0,0 @@
-﻿String name;
-string password;
-Console.WriteLine("Hello, World!");
-Dictionary<string, string> login = new Dictionary<string, string>();
-
-
---
+++ login.cs
@@ -0,0 +1,36 @@
+﻿string[] filen = File.ReadAllLines("users.csv");
+String name;
+String password;
+Dictionary<string, string> loginlist = new Dictionary<string, string>();
+
+foreach (string line in filen)
+{
+    filen = line.Split(';');
+    string name2 = filen[0];
+    string password2 = filen[1];
+    loginlist.Add(name2, password2);
+}
+ while  (true) {
+    Console.WriteLine("Please enter your username");
+    string userinputname = Console.ReadLine();
+    Console.WriteLine("Please enter your password");
+    string passwordinput = Console.ReadLine();
+
+
+     if (loginlist.ContainsKey(userinputname) && passwordinput == loginlist[userinputname])
+        {
+
+            Console.WriteLine(userinputname+ " has logged in");
+
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Your credentials do not exist");
+        Console.WriteLine("Please try again");
+            
+            
+    }
+}
+ 
+
