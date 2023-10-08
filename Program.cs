3f2851f335018a95a860bb02bcaa994cf9767480
albin29 <146028773+albin29@users.noreply.github.com>
2023-10-08 22:41:06 +0200

    added registration function

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace test33;
+enum fadisenum
+{
+    login
+}
+public class Program2
+{
+    static void Main(string[] args)
+    {
+        manager managerinstance = new manager();
+        managerinstance.init();
+
+
+        while (true)
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 / Login");
+            Console.WriteLine("2 / Sign up new user");
+            string option = Console.ReadLine();
+            if (option == "1")
+            {
+                managerinstance.login();
+
+            }
+            else if (option == "2")
+            {
+                
+
+                managerinstance.register();
+
+
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Please pick 1 or 2");
+                continue;
+
+            }
+            break;
+
+        }
+
+
+
+    }
+
+
+}
--- login.cs
+++ login.cs
@@ -1,36 +1,103 @@
-﻿string[] filen = File.ReadAllLines("users.csv");
-String name;
-String password;
-Dictionary<string, string> loginlist = new Dictionary<string, string>();
-
-foreach (string line in filen)
+﻿using System.IO;
+using System.Xml.Linq;
+using test33;
+public class manager
 {
-    filen = line.Split(';');
-    string name2 = filen[0];
-    string password2 = filen[1];
-    loginlist.Add(name2, password2);
-}
- while  (true) {
-    Console.WriteLine("Please enter your username");
-    string userinputname = Console.ReadLine();
-    Console.WriteLine("Please enter your password");
-    string passwordinput = Console.ReadLine();
-
-
-     if (loginlist.ContainsKey(userinputname) && passwordinput == loginlist[userinputname])
-        {
-
-            Console.WriteLine(userinputname+ " has logged in");
-
-        break;
-    }
-    else
+    public bool register()
     {
-        Console.WriteLine("Your credentials do not exist");
-        Console.WriteLine("Please try again");
-            
-            
-    }
-}
- 
+        string? passwordinput;
+        while (true)
+        {
+            Console.WriteLine("Please enter your username");
+            string userinputname = Console.ReadLine();
+            if (usernameExists(userinputname))
+            {
+                Console.Clear();
+                Console.WriteLine("Username already exists");
+                continue;
+            }
+            while (true)
+            {
+                Console.WriteLine("Please enter your password");
+                passwordinput = Console.ReadLine();
 
+                if (passwordinput == "")
+                {
+                    Console.WriteLine("No empty password");
+                    continue;
+
+                }
+                break;
+            }
+            Console.WriteLine(userinputname + " Sucessfully created new user");
+            loginlist.Add(userinputname, passwordinput);
+
+            using (StreamWriter sw = File.AppendText("D:\\siktergit\\test33\\users.csv"))
+            {
+
+                sw.WriteLine(userinputname + ";" + passwordinput);
+            }
+            break;
+        }
+        return true;
+
+    }
+    public bool usernameExists(string username)
+    {
+        if (loginlist.ContainsKey(username))
+        {
+            return true;
+        }
+        return false;
+    }
+    public bool login()
+    {
+        while (true)
+        {
+            Console.WriteLine("Please enter your username");
+            string userinputname = Console.ReadLine();
+            Console.WriteLine("Please enter your password");
+            string passwordinput = Console.ReadLine();
+            if (loginlist.ContainsKey(userinputname) && passwordinput == loginlist[userinputname])
+            {
+
+                Console.WriteLine(userinputname + " has logged in");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Your credentials do not exist");
+                Console.WriteLine("Please try again");
+            }
+        }
+        return true;
+    }
+    public void init()
+    {
+        string[] filen = File.ReadAllLines("D:\\siktergit\\test33\\users.csv");
+
+
+        foreach (string line in filen)
+        {
+            if (line == "")
+            {
+                continue;
+            }
+            filen = line.Split(';');
+
+
+
+            string name2 = filen[0];
+            string password2 = filen[1];
+
+            loginlist.Add(name2, password2);
+
+        }
+
+
+    }
+
+    private Dictionary<string, string> loginlist = new Dictionary<string, string>();
+
+
+}
